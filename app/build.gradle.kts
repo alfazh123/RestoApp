5a4c5d56b9f798561f7fb129ff9e2a32ac0706eb
alfazh123 <alfazh291@gmail.com>
2025-03-01 22:28:15 +0700

    update config.yml circleci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
 //    implementation(libs.androidx.constraintlayout)
 //    implementation(libs.androidx.navigation.fragment.ktx)
 //    implementation(libs.androidx.navigation.ui.ktx)
-//    testImplementation(libs.junit)
-//    androidTestImplementation(libs.androidx.junit)
-//    androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 
     // retrofit
 //    implementation(libs.glide)
